dae1f45b921c5fc88198586ea8d29297de41f3e7
Felix Törnqvist <felix.tornqvist@bitio.se>
2022-03-29 21:53:08 +0200

    react-hook-form example

--- test-react-app/src/App.tsx
+++ test-react-app/src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Control, useFieldArray, useForm } from "react-hook-form";
+
+interface FieldValues {
+  user: {
+    firstName: string;
+    lastName: string;
+    hobbies: { name: string }[];
+  };
+}
 
 function App() {
+  const { register, handleSubmit, control } = useForm<FieldValues>({
+    defaultValues: {
+      user: {
+        firstName: "",
+        lastName: "",
+
+        hobbies: [],
+      },
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "user.hobbies",
+  });
+
+  const onSubmit = (values: FieldValues) => {
+    console.log("You submitted: ", values);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input placeholder="firstname" {...register("user.firstName")} />
+        </div>
+        <div>
+          <input placeholder="lastname" {...register("user.lastName")} />
+          {/* <input placeholder="lastname" {...register("user.last_name")} /> <---- Typescript error! */}
+        </div>
+
+        {fields.map((field, index) => (
+          <div>
+            <input
+              placeholder="hobby"
+              key={field.id}
+              {...register(`user.hobbies.${index}.name`)}
+            />
+            <button
+              onClick={(eve) => {
+                eve.preventDefault();
+                remove(index);
+              }}
+            >
+              Remove hobby
+            </button>
+          </div>
+        ))}
+
+        <button
+          onClick={(eve) => {
+            eve.preventDefault();
+            append({ name: "" });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add hobby
+        </button>
+      </form>
     </div>
   );
 }
